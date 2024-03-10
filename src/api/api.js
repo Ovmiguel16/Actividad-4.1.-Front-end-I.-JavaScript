65715607a0d72c47bed5b84e93adfb51ebdf6075
richardsiv <richardsiverioww@gmail.com>
2024-03-10 15:12:51 -0400

    Token y Key de acceso a la API

---
+++ src/api/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+const API_KEY = '98afb7ea157a87d81ec88a6ff53a386a';
+const API_ACCESS_TOKEN = `eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5OGFmYjdlYTE1N2E4N2Q4MWVjODhhNmZmNTNhMzg2YSIsInN1YiI6IjY0MmIzY2E3ZjFiNTcxMDBmNGMxNDJhNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.U-p5IXtBi_ll9Vh8MPZCGX5cQ9g4fJBX3hiqDu0KFhw`;
+
