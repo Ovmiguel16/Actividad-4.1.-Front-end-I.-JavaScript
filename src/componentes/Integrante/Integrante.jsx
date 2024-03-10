ec5e67f133c43931ff227126104ea616d6708d83
YeriagnyC <chirinosteranyeve@uvm.edu.ve>
2024-03-10 15:06:03 -0400

    Componente Integrante/Nosotros

---
+++ src/componentes/Integrante/Integrante.jsx
@@ -0,0 +1,22 @@
+import style from './Integrante.module.css';
+import Alumno from '../../assets/alumno.png'
+import Alumna from '../../assets/alumna.png'
+
+function Integrante({alumno}) {
+  const colorDelTema = localStorage.getItem('tema');
+
+  return (
+    <div className={`${style.card} ${colorDelTema === 'oscuro' ? style.oscuro : ''}`}>
+        <div className={style.avatar}>
+            <img src={alumno.genero === 'femenino' ? Alumna : Alumno} alt="icono de usuario" />
+        </div>
+        <div className={`${style.infoAlumno} ${colorDelTema === 'oscuro' ? style.oscuro : ''}`}>
+            <h3>{alumno.nombre}</h3>
+            <span><b>Github:</b> {alumno.usuarioGithub}</span>
+            <span><b>Correo:</b> {alumno.usuarioGithub + 'gmail.com'}</span>
+        </div>
+    </div>
+  )
+}
+
+export default Integrante
