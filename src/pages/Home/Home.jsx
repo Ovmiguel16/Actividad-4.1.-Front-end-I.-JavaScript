3cff6ed93a21e6f145bffdeb194f1ff25f47c437
RosangelBetancourt <betancourtmatuzalenrove@uvm.edu.ve>
2024-03-10 16:16:13 -0400

    Efectos de la pagina Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,6 +11,22 @@ function Home() {
   const [peliculasBuscada, setPeliculaBuscada] = useState([])
 
   const peliculas_a_mostrar = peliculasBuscada.length ? peliculasBuscada : peliculas;
+
+  useEffect(() => {
+    peliculasPopularesAction(setLoading, setPeliculas);
+  }, [])
+  
+  useEffect(() => { // Busqueda Debounce
+    const buscarPeliculaDebounce = setTimeout(() => {
+      if( peliculasBuscada.length && queryMovie === '') { // Si borran la busqueda, mostrar las peliculas que estaban
+        setPeliculaBuscada([])
+      }else {
+        buscadorPeliculaAction(queryMovie, setPeliculaBuscada);
+      }
+    }, 1000);
+
+    return () => clearTimeout(buscarPeliculaDebounce)
+  }, [queryMovie])
   
 
   return (
